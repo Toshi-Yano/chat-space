180e213a0d8eedce52f5501a41e0cadc829b8eb9
Toshi-Yano <62419040+Toshi-Yano@users.noreply.github.com>
2020-03-31 21:54:22 +0900

    add-JS
    almost done

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,64 @@
+$(function(){
+
+  var upp = function(upper) {
+    var upper = `<div class="chat-main__list__top">` +
+              `<div class="chat-main__list__top__name">` +
+              message.user_name +
+              `</div>` +
+              `<div class="chat-main__list__top__date">` +
+              message.created_at +
+              `</div>` +
+              `</div>`
+  };
+
+  function buildHTML(message){
+    $(function(upper) {
+    if (message.content && message.image) {
+    var html =  upper + 
+                `<div class="chat-main__list__text">` +
+                `<p class="chat-main__list__text">` +              
+                message.content +
+                `</p>` +
+                `<img src="` + message.image + `"chat-main__list__image" >` +
+                `</div>`
+    } else if (message.content) {
+      var html = upper +
+                 `<div class="chat-main__list__text">` +
+                 `<p class="chat-main__list__text">` +              
+                 message.content +
+                 `</p>` +
+                 `</div>`
+    } else if (message.image) {
+      var html = upper +
+                `<div class="chat-main__list__text">` +
+                `<img src="` + message.image + `"class = "chat-main__list__image" >` +
+                `</div>`
+    };
+    return html;
+    });
+  }
+
+
+  $("#new_message").on("submit", function(e){
+    e.preventDefault()
+    var formData = new FormData(this); 
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data) {
+      var html = buildHTML(data);
+      $('.chat-main__list').append(html).animate({ scrollTop: $('.chat-main__list')[0].scrollHeight});
+      $('form')[0].reset();
+      $('.submit-btn').prop('disabled', false);
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    });
+  })
+});
