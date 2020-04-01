34794738fb4a9e03a8b1f090cf6463e7e5d50009
Toshi-Yano <62419040+Toshi-Yano@users.noreply.github.com>
2020-04-01 15:36:25 +0900

    refactoring

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,14 +1,14 @@
 $(function(){
 
   function upper(message) {
-  var upper_message =  `<div class="chat-main__list__top">
-                        <div class="chat-main__list__top__name">
-                        ${message.user_name}
-                        </div>
-                        <div class="chat-main__list__top__date">
-                        ${message.created_at}
-                        </div>
-                        </div>`;
+    var upper_message =  `<div class="chat-main__list__top">
+                          <div class="chat-main__list__top__name">
+                          ${message.user_name}
+                          </div>
+                          <div class="chat-main__list__top__date">
+                          ${message.created_at}
+                          </div>
+                          </div>`;
     return upper_message;
   };
 
@@ -23,17 +23,17 @@ $(function(){
               <img src=" ${message.image} "chat-main__list__image" >
               </div>`
     } else if (message.content) {
-      html = `${upp}
-              <div class="chat-main__list__text">
-              <p class="chat-main__list__text">
-              ${message.content}
-              </p>
-              </div>`
+        html = `${upp}
+                <div class="chat-main__list__text">
+                <p class="chat-main__list__text">
+                ${message.content}
+                </p>
+                </div>`
     } else if (message.image) {
-      html = `${upp}
-              <div class="chat-main__list__text">
-              <img src=" ${message.image} "class = "chat-main__list__image" >
-              </div>`
+        html = `${upp}
+                <div class="chat-main__list__text">
+                <img src=" ${message.image} "class = "chat-main__list__image" >
+                </div>`
     };
     return html;
   }
